a88af1fe20ba381784753734af77e32b6cb2dd2f
Andrey <andrey.risukhin@outlook.com>
2023-01-06 14:18:46 -0800

    Furthered logic

--- unifsplit.py
+++ unifsplit.py
@@ -11,6 +11,9 @@ Examples:
 > (0,10,0,30): [[0,0,0,10],[0,0,0,0],[0,0,0,10],[0,0,0,0]]
 > (0,2): [[0,1],[0,0]]
 > (1,2,3): [[0,0,1],[0,0,0],[0,0,0]]
+> (0,3,3): [[0,1,1],[0,0,0],[0,0,0]]  # Case when an underpayer needs to repay 2+ people
+> (0,0,1,7): [[0,0,0,2],[0,0,0,2],[0,0,0,1],[0,0,0,0]] # Case when an underpayer already paid an amount
+> () # Case when max payer is at front
 
 Observations:
 - Diagonal is zero
@@ -27,10 +30,18 @@ def unifSplit(a:np.ndarray):
     goal = [fairshare for i in range(n)]
     g = np.array(goal)
 
+    needs_to_pay = g - a # Positive: needs to pay that much, negative: overpayed by that much 
+
+    print(f'DB a: {a}')
+    print(f'DB g: {g}')
+    print(f'DB diff: {needs_to_pay}')
+
+    in_flux = a.copy()
     for ri, row in enumerate(result):
-        if a[ri] < fairshare: # Only for friends that underpaid need to realloc
-            for ci, col_elem in enumerate(row):
-                pass # TODO
+        if a[ri] < fairshare: # Only friends that underpaid need to realloc
+            running_total_payout = 0 # Tracks 
+            for ci, col_elem in enumerate(row): # compare with full row in case overpay was before
+                diff = a[ri] - a[ci]
 
     """
     Logic
@@ -39,3 +50,8 @@ def unifSplit(a:np.ndarray):
 
     # Return as 2d array? elements are how much each 
     pass 
+
+
+in1 = [0,0,0,100]
+
+unifSplit(np.ndarray(in1))
