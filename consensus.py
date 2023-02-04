9ea909f8abde2d18451bfed5e537d80f5bbfef49
Andrey Risukhin Vostro WSL <andrey.risukhin@outlook.com>
2023-02-03 16:43:10 -0800

    consensus project started

---
+++ consensus.py
@@ -0,0 +1,15 @@
+"""
+Example: N friends want to eat at a restaurant. Find the best restaurant given options and personal preferences.
+
+"""
+
+""" 
+Given list of preferences <start with approvals> per person, 
+    and list of restaurants, return ordering of most 
+    acceptable restaurants. 
+Options: [<todo>] M options
+Preferences: [[],[],...] N people's preferences
+    INITIALLY binary approvals, 1 and 0
+"""
+def maxApproval(options, preferences):
+    return 
